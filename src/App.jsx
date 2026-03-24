335b2ef3f21b6b4c04e468fa262c94a4fba99164
GUSTAVO KAULE DOS SANTOS <gustavo_santos178@estudante.sesisenai.org.br>
2026-03-24 14:13:55 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Trabalho Prova</h1>
+    </div>
+  )
+}
+
+export default App
+// ESSE ARQUIVO É PARA AS ROTAS
---
+++ src/pagInicial.jsx
@@ -0,0 +1,10 @@
+// PAGINA QUE VAI MOSTRAR CADA ATIVDADE
+function pagInicial() {
+  return (
+    <div className="App">
+      <h1>Trabalho Prova</h1>
+    </div>
+  )
+}
+
+export default pagInicial
